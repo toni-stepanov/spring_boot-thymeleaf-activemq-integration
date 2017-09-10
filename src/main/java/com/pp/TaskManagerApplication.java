e06ca4c4c490c7c6ad767b779d0fffdebf0ebfdf
Anton Stepanov <toni.stepanof@gmail.com>
2017-09-10 13:53:56 +0300

    add ActiveMQ integration

--- src/main/java/com/pp/TaskManagerApplication.java
+++ src/main/java/com/pp/TaskManagerApplication.java
@@ -1,9 +1,15 @@
 package com.pp;
 
+import com.pp.controller.advice.ExceptionHandlerControllerAdvice;
+import com.pp.domain.chat.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
 
 /**
  * Created by astepanov
@@ -11,8 +17,15 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class TaskManagerApplication extends SpringBootServletInitializer {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerControllerAdvice.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(TaskManagerApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(TaskManagerApplication.class, args);
+        //TODO move it
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+        // Send a message with a POJO - the template reuse the message converter
+        LOGGER.debug("Sending an email message.");
+        jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hey"));
 	}
 
 	@Override
