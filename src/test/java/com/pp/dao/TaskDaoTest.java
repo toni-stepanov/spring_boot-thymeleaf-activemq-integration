7e0706c263386f06fe8a78bcd73035c8beccc3f3
Anton Stepanov <toni.stepanof@gmail.com>
2017-10-14 16:56:24 +0300

    update data test

--- src/test/java/com/pp/dao/TaskDaoTest.java
+++ src/test/java/com/pp/dao/TaskDaoTest.java
@@ -1,15 +1,9 @@
 package com.pp.dao;
 
-import com.pp.repository.TaskRepository;
-import com.github.database.rider.core.api.dataset.DataSet;
-import com.github.database.rider.core.api.dataset.ExpectedDataSet;
-import com.google.common.collect.ImmutableMap;
 import com.pp.repository.UserRepository;
 import org.junit.Test;
-import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -18,10 +12,7 @@ import static org.junit.Assert.assertThat;
 public class TaskDaoTest extends AbstractDaoTest<UserRepository>{
 
     @Test
-    @Sql("user.sql")
-    public void ifNoTasksEmptyListShouldBeReturned(){
-        assertThat(dao.findAll(), is(empty()));
+    @Sql("/one_user.sql")
+    public void severalBooksForTheSameAuthorAreReturned() {
     }
-
-
 }
