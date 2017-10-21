71d9266243460697a3af665a60e32dd0c33b2a41
Anton Stepanov <toni.stepanof@gmail.com>
2017-10-21 16:11:23 +0300

    remove unuseful code

--- src/main/java/com/pp/TaskManagerApplication.java
+++ src/main/java/com/pp/TaskManagerApplication.java
@@ -20,12 +20,7 @@ public class TaskManagerApplication extends SpringBootServletInitializer {
     private static Logger LOGGER = LoggerFactory.getLogger(TaskManagerApplication.class);
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(TaskManagerApplication.class, args);
-        //TODO move it
-        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
-        // Send a message with a POJO - the template reuse the message converter
-        LOGGER.debug("Sending an email message.");
-        jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hey"));
+		SpringApplication.run(TaskManagerApplication.class, args);
 	}
 
 	@Override
